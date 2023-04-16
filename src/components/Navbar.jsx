ed4332790f803af8fd26cb99a169876cc1b21c20
Ayush Desai <ayushsdesai97@gmail.com>
2023-04-17 01:56:58 +0530

    Added logout
    still havent done the redirect to logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Add from "../img/logout.png"
+import React from "react";
+import Add from "../img/logout.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
 const Navbar = () => {
   return (
-    <div className='Navbar'>
-      <span className='logo'>E-MABS</span>
-      <div className='user'>
-        <img src='https://www.patentlyapple.com/.a/6a0120a5580826970c01b8d1a05b87970c-pi' alt=' ' />
+    <div className="Navbar">
+      <span className="logo">E-MABS</span>
+      <div className="user">
+        <img
+          src="https://www.patentlyapple.com/.a/6a0120a5580826970c01b8d1a05b87970c-pi"
+          alt=" "
+        />
         <span>Ayush</span>
-        <button>Log Out</button>
+        <button onClick={() => signOut(auth)}>Log Out</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,26 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
-export const AuthContextProvider = ({ childern }) => {
+
+export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    a;
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      console.log(user);
+    });
+
+    return () => {
+      unsub();
+    };
   }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
