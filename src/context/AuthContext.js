ea10e359a330eee894796c75f4ebe1219cba4ddf
Ayush Desai <ayushsdesai97@gmail.com>
2023-04-17 00:39:10 +0530

    Making a context api for Auth
    Making a context api for Auth

---
+++ src/context/AuthContext.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+export const AuthContextProvider = ({ childern }) => {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    a;
+  }, []);
+};
