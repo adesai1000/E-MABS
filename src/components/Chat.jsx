5553fec92b55a11cfb45fe6735f51c73c43bc37c
Ayush Desai <ayushsdesai97@gmail.com>
2023-04-20 16:19:56 +0530

    can upload any file now
    can upload any file now

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import upload from "../img/upload.png";
 import more from "../img/more.png";
 import download from "../img/download.png";
 import Messages from "./Messages";
 import { AuthContext } from "../context/AuthContext";
 import { db, storage } from "../firebase";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes, listAll,getDownloadURL } from "firebase/storage";
 import { v4 } from "uuid";
 const Chat = () => {
   const { currentUser } = useContext(AuthContext);
   const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, `${currentUser.displayName}`);
   const uploadImage = () => {
     if (imageUpload == null) return;
 
@@ -17,22 +19,37 @@ const Chat = () => {
       storage,
       `${currentUser.displayName}/${imageUpload.name + v4()}`
     );
-    uploadBytes(imageRef, imageUpload).then(() => {
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url)=>{
+        setImageList((prev) =>[...prev,url])
+      })
+      
       alert("Image Uploaded!");
     });
   };
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) =>{
+        getDownloadURL(item).then((url)=>{
+          setImageList((prev) => [...prev,url]);
+        })
+      })
+    });
+  }, []);
   return (
     <div className="Chat">
       <div className="ChatInfo">
         <span>Welcome {currentUser.displayName}</span>
         <div className="ChatIcons">
-          <input
-            type="file"
+          <input 
+            type="file" id="uploadfile" class="hidden"
             onChange={(event) => {
               setImageUpload(event.target.files[0]);
             }}
           />
-          <button onClick={uploadImage}>Upload Image</button>
+          <button onClick={uploadImage}>Upload</button>
+          <button onClick={uploadImage}>Download</button>
+          
         </div>
       </div>
       <Messages />
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import upload from "../img/upload.png";
+import more from "../img/more.png";
+import download from "../img/download.png";
+import Messages from "./Messages";
 import { AuthContext } from "../context/AuthContext";
+import { db, storage } from "../firebase";
+import { getStorage, ref, uploadBytes, listAll,getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
+import Chat from "./Chat"
 const Message = () => {
+  const { currentUser } = useContext(AuthContext);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, `${currentUser.displayName}`);
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) =>{
+        getDownloadURL(item).then((url)=>{
+          setImageList((prev) => [...prev,url]);
+        })
+      })
+    });
+  }, []);
   return (
     <div className="message">
       <div className="messageInfo">
-        <span>Upload your first file by clicking the upload button!</span>
+        <div className="img">{imageList.map((url)=>{
+          return <img src={url} />
+         }  
+         )}</div>
+        
       </div>
     </div>
   );
